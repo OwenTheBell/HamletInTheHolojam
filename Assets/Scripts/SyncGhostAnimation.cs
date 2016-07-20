596d7ce9a8c30453017bbaac9b9be4ae9f37f607
Owen Bell <owenalexbell@gmail.com>
2016-07-20 13:43:02 -0400

    First version of Hamlet

---
+++ Assets/Scripts/SyncGhostAnimation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Holojam;
+using System.Collections;
+
+public class SyncGhostAnimation : Synchronizable {
+
+	private int mRunning = 0;
+
+	protected override void Sync() {
+		if (!sending) {
+			if (synchronizedInt == 1 || mRunning != synchronizedInt) {
+				Debug.Log("I have received the message");
+				GetComponentInChildren<Animator>().SetTrigger("Begin");
+				GetComponent<AudioSource>().Play();
+
+				mRunning = synchronizedInt;
+			}
+		}
+	} 
+
+}
---
+++ Assets/Scripts/TriggerGhostAnimation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Holojam;
+using System.Collections;
+
+public class TriggerGhostAnimation :  Synchronizable {
+
+	public KeyCode Key;
+
+	protected override void Sync() {
+		if (sending && Input.GetKeyDown(Key)) {
+			Debug.Log("BEGIN!!!");
+			synchronizedInt = 1;
+		}
+	}
+
+}
